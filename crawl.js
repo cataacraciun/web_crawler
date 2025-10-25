17b1c77132bbb92d50306bfc4a74e4f63f394c24
Maria Catalina Craciun <cataacraciun@yahoo.com>
2025-10-25 19:59:59 +0300

    normalizeURL and tests

---
+++ crawl.js
@@ -0,0 +1,14 @@
+function normalizeURL(urlString) {
+    const urlObj = new URL(urlString)
+    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
+
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+        return hostPath.slice(0, -1)
+    }
+    return hostPath
+}
+
+// THIS IS CRITICAL: It makes the function available for other files to import.
+module.exports = {
+    normalizeURL
+}
